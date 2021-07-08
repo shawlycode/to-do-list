40ab2b20a59f8ce7881050becb6fe7a5d2d32dd2
Shawlycode <kutinaorlando@gmail.com>
2021-07-08 19:45:27 +0000

    updated components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
+import './App.css'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      newItem:"",
-      list:[]
-    }
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+  addItem() {
+    const newItem = {
+      //create item with unique id
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+    //copy of list of items
+    const list = [...this.state.list];
+    //add new items to list
+
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem: "",
+    });
+  }
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updateList = list.filter((item) => item.id !== id);
+    this.setState({ list: updateList });
   }
   render() {
-    return <div></div>;
+    return (
+      <div className="App">
+        <div>
+          <h3>Add an item...</h3>
+          <br />
+          <input
+            type="text"
+            placeholder="type item here..."
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput("newItem", e.target.value)}
+          ></input>
+          <button onClick={() => this.addItem()}>Add</button>
+          <ul>
+            {this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>X</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
